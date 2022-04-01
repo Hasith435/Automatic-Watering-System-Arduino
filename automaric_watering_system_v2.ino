70d303ddc132c33fffbe4cc0694a0a6388377125
Hasith Dewmina <66883843+Hasith435@users.noreply.github.com>
2022-04-01 16:21:31 +0400

    new code

--- automaric_watering_system_v2.ino
+++ automaric_watering_system_v2.ino
@@ -1,24 +1,48 @@
-int sens_1 = A0;
-int sens_2 = A1;
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);
+
+
+int sens_1 = A2;
+int sens_2 = A3;
+int motor_pin = 8;
 
 void setup(){
 
   Serial.begin(9600);
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3c);
+  display.clearDisplay();
   
   pinMode(sens_1, INPUT);
   pinMode(sens_2, INPUT);
+  pinMode(motor_pin, OUTPUT);
 }
 
 void loop(){
-  float sens_1_read = analogRead(sens_1);
-  float sens_2_read = analogRead(sens_2);
+  float sen_1_read = analogRead(sens_1);
+  float sen_2_read = analogRead(sens_2);
 
-  Serial.print("");
-  Serial.print("Sensor 1:");
-  Serial.println(sens_1_read);
-  Serial.print("Sensor 2:");
-  Serial.print(sens_2_read);
-  Serial.println("");
+//  Serial.print("");
+//  Serial.print("Sensor 1:");
+//  Serial.println(sens_1_read);
+//  Serial.print("Sensor 2:");
+//  Serial.print(sens_2_read);
+//  Serial.println("");
 
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  display.print("Sen_1:");
+  display.println(sen_1_read);
+  display.print("Sen_2:");
+  display.print(sen_2_read);
+  display.display();
+  display.clearDisplay();
   delay(1000);
+
+  digitalWrite(motor_pin, HIGH);
 }
